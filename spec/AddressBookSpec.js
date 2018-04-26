fc8a76155ad6e4281e5307c618036530dec047ab
fullyslick <default@mail.com>
2018-04-26 18:01:08 +0300

    Test is deleteContact is working

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -16,4 +16,20 @@ describe('Address Book', function() {
     expect(addressBook.getContact(0)).toBe(thisContact);
     // OR addressBook.getContact(0) === thisContact
   });
+
+  // Define the test.
+  // Type a string with the purpose of test.
+  // Type a function that will be executed to run the test.
+  it('should be able to delete a contact', function() {
+    // Create new address book object
+    var addressBook = new AddressBook();
+    // Create new contact object
+    var thisContact = new Contact();
+    // Add the thisContact object to the addressBook object using defined method in AddressBook prototype
+    addressBook.addContact(thisContact);
+    // Should define 'deleteContact' method in the prototype of AddressBook constructor.
+    addressBook.deleteContact(0);
+    // expect - (the rerun of a function that we are testing).method defined by Jasmine, that will return true or false. In this case check if the return value is something - toBe(something).
+    expect(addressBook.deleteContact(0)).not.toBeDefined();
+  });
 });
