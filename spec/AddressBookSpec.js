af551e4a52673e0dfddada20d792582a31a13c89
fullyslick <default@mail.com>
2018-04-27 16:04:14 +0300

    Execute tasks before running tests

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,18 +1,29 @@
 describe('Address Book', function() {
+  // Global variables accessed by all tests below.
+  var addressBook;
+  var thisContact;
+
+  // Before each test, you can perform some tasks.
+  // For example:
+  // Place here variable assignments or functions that will be accessed by all test.
+  beforeEach(function() {
+    // Create new address book object and assign it to a global variable
+    addressBook = new AddressBook();
+    // Create new contact object and assign it to a global variable
+    thisContact = new Contact();
+  });
+
   // Define the test.
   // Type a string with the purpose of test.
   // Type a function that will be executed to run the test.
   it('should be able to add a contact', function() {
-    // Create new address book object
-    var addressBook = new AddressBook();
-    // Create new contact object
-    var thisContact = new Contact();
+
     // Add the thisContact object to the addressBook object
     // Should define 'addContact' method in the prototype of AddressBook constructor
     addressBook.addContact(thisContact);
     // TEST if 'thisContact' was added to 'addressBook'
     // Should define 'getContact' method in the prototype of AddressBook constructor.
-    // expect - (the rerun of a function that we are testing).method defined by Jasmine, that will return true or false. In this case check if the return value is something - toBe(something).
+    // expect - (the return of a function that we are testing).method defined by Jasmine, that will return true or false. In this case check if the return value is something - toBe(something).
     expect(addressBook.getContact(0)).toBe(thisContact);
     // OR addressBook.getContact(0) === thisContact
   });
@@ -21,15 +32,11 @@ describe('Address Book', function() {
   // Type a string with the purpose of test.
   // Type a function that will be executed to run the test.
   it('should be able to delete a contact', function() {
-    // Create new address book object
-    var addressBook = new AddressBook();
-    // Create new contact object
-    var thisContact = new Contact();
     // Add the thisContact object to the addressBook object using defined method in AddressBook prototype
     addressBook.addContact(thisContact);
     // Should define 'deleteContact' method in the prototype of AddressBook constructor.
     addressBook.deleteContact(0);
-    // expect - (the rerun of a function that we are testing).method defined by Jasmine, that will return true or false. In this case check if the return value is something - toBe(something).
+    // expect - (the return of a function that we are testing).method defined by Jasmine, that will return true or false. In this case check if the return value is something - toBe(something).
     expect(addressBook.getContact(0)).not.toBeDefined();
   });
 });
